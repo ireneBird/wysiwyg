086bcbf06d583b3f18ccc767d41c5e829e54a274
Irina Nesterenko <inesterenko@roox.ru>
2022-12-03 18:13:02 +0100

    Added webpack config

---
+++ webpack.config.ts
@@ -0,0 +1,89 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/main.ts',
+  mode: 'development',
+  devtool: 'inline-source-map', // карта исходников
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+    chunkFilename: '[id].[chunkhash].js',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {},
+          },
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(svg|woff|woff2|ttf|eot|otf)([\?]?.*)$/,
+        use: [
+          {
+            loader: 'file-loader?name=assets/fonts/[name].[ext]',
+          },
+        ],
+      },
+    ],
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      filename: 'index.html',
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+      },
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'style-[hash].css',
+    }),
+    // new CopyWebpackPlugin({
+    //   patterns: [
+    //     {
+    //       from: '**/*',
+    //       context: path.resolve(__dirname, 'src', 'assets'),
+    //       to: './assets',
+    //     },
+    //   ],
+    // }),
+  ],
+
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 9000,
+  },
+
+  watch: true,
+  watchOptions: {
+    ignored: ['/node_modules/'],
+  },
+};
